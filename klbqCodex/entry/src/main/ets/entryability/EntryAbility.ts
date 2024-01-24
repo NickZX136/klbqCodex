5afd88a76b21952b21d9fe6405d14ca55c177352
NickZX136 <1363043892@qq.com>
2024-01-24 22:41:17 +0800

    first commit

---
+++ klbqCodex/entry/src/main/ets/entryability/EntryAbility.ts
@@ -0,0 +1,94 @@
+import UIAbility from '@ohos.app.ability.UIAbility';
+import hilog from '@ohos.hilog';
+import window from '@ohos.window';
+
+let selectPage = "";
+let currentWindowStage = null;
+
+export default class EntryAbility extends UIAbility {
+
+  private characterAddress: Array<string> = [
+    "pages/XiangNaiMei",
+    "pages/MeiRuiDiSi",
+    "pages/LaWei",
+    "pages/Min",
+    "pages/Ling",
+    "pages/MiXueEr",
+    "pages/XinXia",
+    "pages/Xin",
+    "pages/YiWeiTe",
+    "pages/MaDeLeiNa",
+    "pages/FeiSha",
+    "pages/XingHui",
+    "pages/AoDaiLi",
+    "pages/BaiMo"
+  ]
+
+  private randomIndex = Math.floor(Math.random() * this.characterAddress.length);
+  private randomPage: string = this.characterAddress[this.randomIndex];
+
+  onCreate(want, launchParam) {
+    // 获取router事件中传递的targetPage参数
+    console.info("onCreate want:" + JSON.stringify(want));
+    if (want.parameters.params !== undefined) {
+      let params = JSON.parse(want.parameters.params);
+      console.info("onCreate router targetPage:" + params.targetPage);
+      selectPage = params.targetPage;
+    }
+  }
+
+  onNewWant(want, launchParam) {
+    console.info("onNewWant want:" + JSON.stringify(want));
+    if (want.parameters.params !== undefined) {
+      let params = JSON.parse(want.parameters.params);
+      console.info("onNewWant router targetPage:" + params.targetPage);
+      selectPage = params.targetPage;
+    }
+    if (currentWindowStage != null) {
+      this.onWindowStageCreate(currentWindowStage);
+    }
+  }
+
+
+  onDestroy() {
+    hilog.info(0x0000, 'testTag', '%{public}s', 'Ability onDestroy');
+  }
+
+  onWindowStageCreate(windowStage: window.WindowStage) {
+    let targetPage;
+    // 根据传递的targetPage不同，选择拉起不同的页面
+    switch (selectPage) {
+      case 'fun':
+        targetPage = this.randomPage;
+        break;
+      default:
+        targetPage = 'pages/Index';
+    }
+    if (currentWindowStage === null) {
+      currentWindowStage = windowStage;
+    }
+    windowStage.loadContent(targetPage, (err, data) => {
+      if (err && err.code) {
+        console.info('Failed to load the content. Cause: %{public}s', JSON.stringify(err));
+        return;
+      }
+    });
+  }
+
+  onWindowStageDestroy() {
+    // Main window is destroyed, release UI related resources
+    hilog.info(0x0000, 'testTag', '%{public}s', 'Ability onWindowStageDestroy');
+  }
+
+  onForeground() {
+    // Ability has brought to foreground
+    hilog.info(0x0000, 'testTag', '%{public}s', 'Ability onForeground');
+  }
+
+  onBackground() {
+    // Ability has back to background
+    hilog.info(0x0000, 'testTag', '%{public}s', 'Ability onBackground');
+  }
+}
+
+
